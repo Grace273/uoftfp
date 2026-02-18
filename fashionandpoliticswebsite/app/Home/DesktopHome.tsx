32c4636fd995843c91a62c119e990267b2fd1bfd
Grace Wang <84159791+Grace273@users.noreply.github.com>
2026-02-17 19:57:02 -0500

    add mobile homepage

---
+++ fashionandpoliticswebsite/app/Home/DesktopHome.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import Image from "next/image";
+import FeaturedCarousel from "@/components/FeaturedCarousel";
+export default function DesktopHome() {
+  return (
+    <div className="relative h-full sm:py-0 page-transition bg-[#D3D3D3]">
+      <section className="grid md:grid-cols-[auto_1fr]">
+        <div className="relative h-full">
+          <Image
+            src="/images/homepage-girl.png"
+            alt="Model"
+            width={850}
+            height={850}
+            className="object-contain bg-[#D3D3D3] ml-[4vh]"
+          />
+        </div>
+        <div className="flex flex-col justify-center items-center md:mt-[15vh] gap-[7vh] h-[70vh]">
+          <div>
+            <Image
+              src="/images/uoftfp-logo.jpg"
+              alt="UofT Fashion and Politics Club Logo"
+              width={450}
+              height={450}
+              className="object-contain"
+            />
+          </div>
+          <div>
+            <FeaturedCarousel />
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
--- fashionandpoliticswebsite/app/page.tsx
+++ fashionandpoliticswebsite/app/page.tsx
@@ -1,36 +1,18 @@
 "use client";
 
-import Image from "next/image";
-import FeaturedCarousel from "@/components/FeaturedCarousel";
+import DesktopHome from "./Home/DesktopHome";
+import MobileHome from "./Home/MobileHome";
 
 export default function HomePage() {
   return (
-    <div className="relative h-full sm:py-0 page-transition bg-[#D3D3D3]">
-      <section className="grid md:grid-cols-[auto_1fr]">
-        <div className="relative h-full">
-          <Image
-            src="/images/homepage-girl.png"
-            alt="Model"
-            width={850}
-            height={850}
-            className="bg-[#D3D3D3] ml-[4vh]"
-          />
-        </div>
-        <div className="flex flex-col justify-center items-center md:mt-[15vh] gap-[7vh] h-[70vh]">
-          <div>
-            <Image
-              src="/images/uoftfp-logo.jpg"
-              alt="UofT Fashion and Politics Club Logo"
-              width={450}
-              height={450}
-              className="object-contain"
-            />
-          </div>
-          <div>
-            <FeaturedCarousel />
-          </div>
-        </div>
-      </section>
-    </div>
+    <>
+      <div className="hidden md:block">
+        <DesktopHome />
+      </div>
+
+      <div className="block md:hidden">
+        <MobileHome />
+      </div>
+    </>
   );
 }
