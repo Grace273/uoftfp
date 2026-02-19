cd04dc5f82dadd2ecdf26856e817b24d08c77d9d
Grace Wang <84159791+Grace273@users.noreply.github.com>
2026-02-18 21:28:06 -0500

    update nav spacing

--- fashionandpoliticswebsite/components/Navbar/MobileNav.tsx
+++ fashionandpoliticswebsite/components/Navbar/MobileNav.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function MobileNav() {
   return (
-    <nav className="flex items-center px-[5%] font-helvetica bg-[#d2d2d2]">
+    <nav className="flex items-center px-[2%] font-helvetica bg-[#d2d2d2]">
       {/* Monogram on the left */}
       <Link href="/">
         <Image
