a2912306335ed7c98e2dc6c4b3961a4824792306
Grace Wang <84159791+Grace273@users.noreply.github.com>
2026-02-19 15:31:14 -0500

    update about

--- fashionandpoliticswebsite/app/about/DesktopAbout.tsx
+++ fashionandpoliticswebsite/app/about/DesktopAbout.tsx
@@ -77,7 +77,7 @@ export default function AboutPage() {
                 alt=""
                 width={700}
                 height={900}
-                className="mb-6 rotate-[0deg] shadow-lg"
+                className="mb-6 rotate-[1deg] shadow-lg"
               />
 
               <Image
@@ -85,7 +85,7 @@ export default function AboutPage() {
                 alt=""
                 width={700}
                 height={900}
-                className="mb-6 rotate-[0deg] shadow-lg"
+                className="mb-6 rotate-[-1deg] shadow-lg"
               />
 
               <Image
@@ -93,7 +93,7 @@ export default function AboutPage() {
                 alt=""
                 width={700}
                 height={900}
-                className="mb-6 rotate-[0deg] shadow-lg"
+                className="mb-6 rotate-[-1deg] shadow-lg"
               />
 
               <Image
@@ -101,7 +101,7 @@ export default function AboutPage() {
                 alt=""
                 width={700}
                 height={900}
-                className="mb-6 rotate-[0deg] shadow-lg"
+                className="mb-6 rotate-[1deg] shadow-lg"
               />
 
               <Image
@@ -109,7 +109,7 @@ export default function AboutPage() {
                 alt=""
                 width={700}
                 height={900}
-                className="mb-6 rotate-[0deg] shadow-lg"
+                className="mb-6 rotate-[1deg] shadow-lg"
               />
             </div>
           </div>
@@ -131,7 +131,7 @@ export default function AboutPage() {
                   alt=""
                   width={700}
                   height={900}
-                  className="mb-6 rotate-[0deg] shadow-lg"
+                  className="mb-6 rotate-[1deg] shadow-lg"
                 />
 
                 <Image
@@ -139,7 +139,7 @@ export default function AboutPage() {
                   alt=""
                   width={700}
                   height={900}
-                  className="mb-6 rotate-[0deg] shadow-lg"
+                  className="mb-6 rotate-[-1deg] shadow-lg"
                 />
 
                 <Image
@@ -147,7 +147,7 @@ export default function AboutPage() {
                   alt=""
                   width={700}
                   height={900}
-                  className="mb-6 rotate-[0deg] shadow-lg"
+                  className="mb-6 rotate-[-1deg] shadow-lg"
                 />
                 <br></br>
                 <br></br>
