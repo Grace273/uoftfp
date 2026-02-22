6864ae8838afc2e999cbdb2c5fd9976a2b960978
Grace Wang <84159791+Grace273@users.noreply.github.com>
2026-02-21 22:58:43 -0500

    update

--- fashionandpoliticswebsite/components/Navbar/DesktopMenu.tsx
+++ fashionandpoliticswebsite/components/Navbar/DesktopMenu.tsx
@@ -12,14 +12,13 @@ export default function DesktopMenu({ isOpen, setIsOpen }: Props) {
     <>
       {/* Slide-in Menu */}
       <div
-        //  w-64 before
-        className={`fixed top-0 left-0 h-full w-47 bg-[#d2d2d2] text-[#1c1c1c] shadow-lg z-40 transform transition-transform duration-300 ${
+        className={`fixed top-0 left-0 h-full w-36 bg-[#d2d2d2] text-[#1c1c1c] shadow-lg z-40 transform transition-transform duration-300 ${
           isOpen ? "translate-x-0" : "-translate-x-full"
         }`}
       >
         {/* ml-0 before */}
         <div className="h-full flex flex-col justify-between">
-          <div className="flex flex-col items-center mt-20 gap-13 text-lg">
+          <div className="flex flex-col items-center ml-[-1vh] mt-20 gap-13 text-lg">
             <Link href="/">
               <Image
                 src="/images/monogram.png"
